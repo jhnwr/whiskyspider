e287223c01213a3f1fd2f0d5bc54099a3f1e730d
John <50677710+jhnwr@users.noreply.github.com>
2020-12-29 07:00:37 +0000

    Update items.py
    updated for items and itemloader

--- items.py
+++ items.py
@@ -4,9 +4,15 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
 
+def remove_currency(value):
+    return value.replace('£','').strip()
 
 class WhiskyscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    price = scrapy.Field(input_processor = MapCompose(remove_tags, remove_currency), output_processor = TakeFirst())
+    link = scrapy.Field()
